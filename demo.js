47d73b606d63318e04b33126b85c5d77d75194c2
Chong130 <112062203+Chong130@users.noreply.github.com>
2024-01-07 00:47:12 +0800

    Update demo.js

--- demo.js
+++ demo.js
@@ -2,13 +2,13 @@ function startConnect(){
 
     clientID = "clientID - "+parseInt(Math.random() * 100);
 
-    host = document.getElementById("host").value;   
-    port = document.getElementById("port").value;  
-    userId  = document.getElementById("username").value;  
-    passwordId = document.getElementById("password").value;  
+    // host = document.getElementById("host").value;   
+    // port = document.getElementById("port").value;  
+    // userId  = document.getElementById("username").value;  
+    // passwordId = document.getElementById("password").value;  
 
-    document.getElementById("messages").innerHTML += "<span> Connecting to " + host + "on port " +port+"</span><br>";
-    document.getElementById("messages").innerHTML += "<span> Using the client Id " + clientID +" </span><br>";
+    // document.getElementById("messages").innerHTML += "<span> Connecting to " + host + "on port " +port+"</span><br>";
+    // document.getElementById("messages").innerHTML += "<span> Using the client Id " + clientID +" </span><br>";
 
     // client = new Paho.MQTT.Client(host,Number(port),clientID);
     client = new Paho.MQTT.Client("wss://mqtt-dashboard.com:8884/mqtt" ,clientID);
@@ -35,7 +35,7 @@ function onConnect(){
     topic =  "testtopic/userpw";
     
 
-    document.getElementById("messages").innerHTML += "<span> Subscribing to topic "+topic + "</span><br>";
+    // document.getElementById("messages").innerHTML += "<span> Subscribing to topic "+topic + "</span><br>";
 
     // console.log(topic)
     client.subscribe(topic);
@@ -45,20 +45,20 @@ function onConnect(){
 
 
 function onConnectionLost(responseObject){
-    document.getElementById("messages").innerHTML += "<span> ERROR: Connection is lost.</span><br>";
+    // document.getElementById("messages").innerHTML += "<span> ERROR: Connection is lost.</span><br>";
     if(responseObject !=0){
-        document.getElementById("messages").innerHTML += "<span> ERROR:"+ responseObject.errorMessage +"</span><br>";
+        // document.getElementById("messages").innerHTML += "<span> ERROR:"+ responseObject.errorMessage +"</span><br>";
     }
 }
 
 function onMessageArrived(message){
     // console.log("OnMessageArrived: "+message.payloadString);
-    document.getElementById("messages").innerHTML += "<span> Topic:"+message.destinationName+"| Message : "+message.payloadString + "</span><br>";
+    // document.getElementById("messages").innerHTML += "<span> Topic:"+message.destinationName+"| Message : "+message.payloadString + "</span><br>";
 }
 
 function startDisconnect(){
     client.disconnect();
-    document.getElementById("messages").innerHTML += "<span> Disconnected. </span><br>";
+    // document.getElementById("messages").innerHTML += "<span> Disconnected. </span><br>";
 
 
 
@@ -76,7 +76,7 @@ Message = new Paho.MQTT.Message(msg);
 Message.destinationName = topic;
 
 client.send(Message);
-document.getElementById("messages").innerHTML += "<span> Message to topic "+topic+" is sent </span><br>";
+// document.getElementById("messages").innerHTML += "<span> Message to topic "+topic+" is sent </span><br>";
 
 
 }
